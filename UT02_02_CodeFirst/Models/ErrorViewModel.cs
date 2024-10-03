345f0e5a771e206816bc8609425c937a9c75bbb6
2dawa <2dawa@informatica.edu>
2024-10-03 09:28:40 +0100

    Initial commit

---
+++ UT02_02_CodeFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UT02_02_CodeFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
