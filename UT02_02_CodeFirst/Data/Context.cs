72df3f565ce28c72ac188a93272a216877fa690f
2dawa <2dawa@informatica.edu>
2024-10-04 09:56:34 +0100

    Seeder created

---
+++ UT02_02_CodeFirst/Data/Context.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UT02_02_CodeFirst.Models;
+
+namespace CodeFirst.Data
+{
+    public class Context : DbContext
+    {
+        public Context (DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UT02_02_CodeFirst.Models.Car> Car { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Car>().HasData(
+                new Car {
+                    Id=1,
+                    Model="Panda",
+                    Brand="Seat",
+                    ReleaseDate=DateTime.Now,
+                    Price=15000
+                },
+                  new Car
+                  {
+                      Id = 2,
+                      Model = "Ibiza",
+                      Brand = "Seat",
+                      ReleaseDate = DateTime.Now,
+                      Price = 16000
+                  },
+                    new Car
+                    {
+                        Id = 3,
+                        Model = "Franchesco",
+                        Brand = "Audi",
+                        ReleaseDate = DateTime.Now,
+                        Price = 20000
+                    }
+
+                );
+        }
+
+
+    }
+}
---
+++ UT02_02_CodeFirst/Migrations/20241004084735_Seeder3car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace UT02_02_CodeFirst.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seeder3car : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Car",
+                columns: new[] { "Id", "Brand", "Model", "Price", "ReleaseDate" },
+                values: new object[,]
+                {
+                    { 1, "Seat", "Panda", 15000m, new DateTime(2024, 10, 4, 9, 47, 34, 996, DateTimeKind.Local).AddTicks(3978) },
+                    { 2, "Seat", "Ibiza", 16000m, new DateTime(2024, 10, 4, 9, 47, 34, 996, DateTimeKind.Local).AddTicks(4025) },
+                    { 3, "Audi", "Franchesco", 20000m, new DateTime(2024, 10, 4, 9, 47, 34, 996, DateTimeKind.Local).AddTicks(4028) }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Car",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Car",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Car",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
